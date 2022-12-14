3aed8f139974e296b8010ef8de2bdb38e200a80a
Thomas Marchand <thomas.marchand@tuta.io>
2022-12-14 18:21:48 +0100

    update: contracts deployment

--- deploy.py
+++ deploy.py
@@ -2,9 +2,7 @@ from starkware.starknet.compiler.compile import get_selector_from_name
 from starknet_py.net.models.chains import StarknetChainId
 from starknet_py.net import AccountClient, KeyPair
 from starknet_py.net.gateway_client import GatewayClient
-from starknet_py.net.networks import Network
-from starknet_py.transactions.deploy import make_deploy_tx
-from starknet_py.compile.compiler import create_contract_class
+from starknet_py.contract import Contract
 import asyncio
 import sys
 
@@ -14,14 +12,59 @@ deployer_account_addr = (
     0x048F24D0D0618FA31813DB91A45D8BE6C50749E5E19EC699092CE29ABE809294
 )
 deployer_account_private_key = int(argv[1])
+universal_deployer = 0x041A78E741E5AF2FEC34B695679BC6891742439F7AFB8484ECD7766661AD02BF
 token = argv[2] if len(argv) > 2 else None
 admin = 0x048F24D0D0618FA31813DB91A45D8BE6C50749E5E19EC699092CE29ABE809294
-# MAINNET: https://alpha-mainnet.starknet.io
-# TESTNET: https://alpha4.starknet.io
-# TESTNET2: https://alpha4-2.starknet.io
-network_base_url = "https://alpha-mainnet.starknet.io/"
-chainid: StarknetChainId = StarknetChainId.MAINNET
+# MAINNET: https://alpha-mainnet.starknet.io/
+# TESTNET: https://alpha4.starknet.io/
+# TESTNET2: https://alpha4-2.starknet.io/
+network_base_url = "https://alpha4.starknet.io/"
+chainid: StarknetChainId = StarknetChainId.TESTNET
 max_fee = int(1e16)
+
+DEPLOYER_ABI = [
+    {
+        "data": [
+            {"name": "address", "type": "felt"},
+            {"name": "deployer", "type": "felt"},
+            {"name": "unique", "type": "felt"},
+            {"name": "classHash", "type": "felt"},
+            {"name": "calldata_len", "type": "felt"},
+            {"name": "calldata", "type": "felt*"},
+            {"name": "salt", "type": "felt"},
+        ],
+        "keys": [],
+        "name": "ContractDeployed",
+        "type": "event",
+    },
+    {
+        "name": "deployContract",
+        "type": "function",
+        "inputs": [
+            {"name": "classHash", "type": "felt"},
+            {"name": "salt", "type": "felt"},
+            {"name": "unique", "type": "felt"},
+            {"name": "calldata_len", "type": "felt"},
+            {"name": "calldata", "type": "felt*"},
+        ],
+        "outputs": [{"name": "address", "type": "felt"}],
+    },
+]
+
+
+async def deploy_contract(
+    account_client, class_hash, constructor, salt=0, unique=0
+) -> int:
+    contract = Contract(
+        universal_deployer,
+        DEPLOYER_ABI,
+        account_client,
+    )
+
+    invocation = await contract.functions["deployContract"].invoke(
+        class_hash, salt, unique, constructor, max_fee=max_fee
+    )
+    return invocation.hash
 
 
 async def main():
@@ -38,32 +81,41 @@ async def main():
         chain=chainid,
         supported_tx_version=1,
     )
-    logic_file = open("./build/logic.json", "r")
+    impl_file = open("./build/starknetid.json", "r")
     declare_contract_tx = await account.sign_declare_transaction(
-        compiled_contract=logic_file.read(), max_fee=max_fee
+        compiled_contract=impl_file.read(), max_fee=max_fee
     )
-    logic_file.close()
-    logic_declaration = await client.declare(
+    impl_file.close()
+    impl_declaration = await client.declare(
         transaction=declare_contract_tx, token=token
     )
-    logic_contract_class_hash = logic_declaration.class_hash
-    print("implementation class hash:", hex(logic_contract_class_hash))
+    impl_contract_class_hash = impl_declaration.class_hash
+    print("implementation class hash:", hex(impl_contract_class_hash))
 
     proxy_file = open("./build/proxy.json", "r")
-    deploy_contract_tx = make_deploy_tx(
-        compiled_contract=create_contract_class(proxy_file.read()),
-        constructor_calldata=[
-            logic_contract_class_hash,
+    declare_contract_tx = await account.sign_declare_transaction(
+        compiled_contract=proxy_file.read(), max_fee=max_fee
+    )
+    proxy_file.close()
+    proxy_declaration = await client.declare(
+        transaction=declare_contract_tx, token=token
+    )
+    proxy_contract_class_hash = proxy_declaration.class_hash
+    print("proxy class hash:", hex(proxy_contract_class_hash))
+
+    tx_hash = await deploy_contract(
+        account,
+        proxy_contract_class_hash,
+        constructor=[
+            impl_contract_class_hash,
             get_selector_from_name("initializer"),
             1,
             admin,
         ],
-        version=1,
     )
-    proxy_file.close()
-    deployment_resp = await client.deploy(transaction=deploy_contract_tx, token=token)
-    print("deployment txhash:", hex(deployment_resp.transaction_hash))
-    print("proxied contract address:", hex(deployment_resp.contract_address))
+
+    print("deployment txhash:", hex(tx_hash))
+    # print("proxied contract address:", hex(deployment_resp.contract_address))
 
 
 if __name__ == "__main__":
